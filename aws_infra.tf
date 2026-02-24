40ccca4a0dad142e9c5a83ba7f44d1a22353603c
HarshaVardhannaidu07 <96299815+HarshaVardhannaidu07@users.noreply.github.com>
2026-02-24 21:43:14 +0530

    Create AWS infrastructure with VPC and EC2 instances
    This Terraform configuration sets up AWS infrastructure including a VPC, subnets, security groups, EC2 instances, and associated resources.

---
+++ aws_infra.tf
@@ -0,0 +1,136 @@
+provider "aws" {
+
+region = "ap-southeast-2"
+}
+
+resource "tls_private_key" "mykey" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "pem_file" {
+  content  = tls_private_key.mykey.private_key_pem
+  filename = "lab.pem"
+  file_permission = "0400"
+}
+
+
+resource "aws_key_pair" "aws_key" {
+  key_name   = "lab-key"
+  public_key = tls_private_key.mykey.public_key_openssh
+}
+
+
+resource "aws_vpc" "sl-vpc" {
+ cidr_block = "10.0.0.0/16"
+  tags = {
+   Name = "sl-vpc"
+}
+}
+resource "aws_subnet" "subnet-1"{
+
+vpc_id = aws_vpc.sl-vpc.id
+cidr_block = "10.0.1.0/24"
+depends_on = [aws_vpc.sl-vpc]
+map_public_ip_on_launch = true
+  tags = {
+   Name = "sl-subnet"
+}
+}
+resource "aws_route_table" "sl-route-table"{
+vpc_id = aws_vpc.sl-vpc.id
+  tags = {
+   Name = "sl-route-table"
+}
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.sl-route-table.id
+}
+
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.sl-vpc.id
+ depends_on = [aws_vpc.sl-vpc]
+   tags = {
+   Name = "sl-gw"
+}
+
+}
+
+resource "aws_route" "sl-route" {
+
+route_table_id = aws_route_table.sl-route-table.id
+destination_cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.gw.id
+
+
+}
+
+variable "sg_ports" {
+type = list(number)
+default = [8080,80,22,443]
+}
+
+resource "aws_security_group" "sl-sg" {
+  name        = "sg_rule"
+  vpc_id = aws_vpc.sl-vpc.id
+  dynamic  "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content{
+    from_port        = port.value
+    to_port          = port.value
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+  }
+egress {
+
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+
+}
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0c73bd9145b5546f5"
+  instance_type = "m7i-flex.large"
+  key_name = aws_key_pair.aws_key.key_name
+  subnet_id = aws_subnet.subnet-1.id
+  security_groups = [aws_security_group.sl-sg.id]
+  tags = {
+    Name = "Project-Dev"
+  }
+
+}
+
+
+resource "aws_instance" "myec2-test" {
+ ami           = "ami-0c73bd9145b5546f5"
+  instance_type = "m7i-flex.large"
+  key_name = aws_key_pair.aws_key.key_name
+  subnet_id = aws_subnet.subnet-1.id
+  security_groups = [aws_security_group.sl-sg.id]
+  tags = {
+    Name = "Project-Test"
+  }
+
+}
+
+resource "aws_instance" "myec2-prod" {
+  ami           = "ami-0c73bd9145b5546f5"
+  instance_type = "m7i-flex.large"
+  key_name = aws_key_pair.aws_key.key_name
+  subnet_id = aws_subnet.subnet-1.id
+  security_groups = [aws_security_group.sl-sg.id]
+  tags = {
+    Name = "Project-Prod"
+  }
+
+}
+
